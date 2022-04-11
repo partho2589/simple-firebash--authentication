e0b1e8b9af318e748441bd5b7a249a64abd0fa0b
partho2589 <parthomalo615@.com>
2022-04-12 05:22:45 +0600

    add button

--- src/App.js
+++ src/App.js
@@ -1,11 +1,24 @@
 import './App.css';
-import {getAuth } from 'firebase/auth'
+import { getAuth, GoogleAuthProvider, signInWithPopup } from 'firebase/auth'
 import app from './firebase.init';
-const  auth = getAuth(app);
+const auth = getAuth(app);
 function App() {
+  const provider = new GoogleAuthProvider();
+  const handelGoogleSingIn = () => {
+   signInWithPopup(auth, provider)
+   .then(result => {
+     const user = result.user
+     console.log(user)
+   })
+   .catch(error => {
+    console.log(error)
+   })
+   
+  }
+
   return (
     <div className="App">
-     
+      <button onClick={handelGoogleSingIn}>Google Sing In </button>
     </div>
   );
 }
